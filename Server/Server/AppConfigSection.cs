c674c3001ba4b9124af80f7ee97ba88128218e9d
Abdul Karim <akashcse@gmail.com>
2024-01-01 16:03:49 +0600

    Add files via upload
    Initial Upload

---
+++ Server/Server/AppConfigSection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class AppConfigSection
+    {
+
+        private AppConfigSection()
+        {
+
+
+
+
+
+        }
+
+        public static string IP
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["IP"].ToString();
+            }
+        }
+        public static int Port { get { return int.Parse(System.Configuration.ConfigurationManager.AppSettings["Port"]); } }
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+        public static string From { get; set; }
+        public static bool EnableSsl { get; set; }
+    }
+}
---
+++ Server/Server/LogWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+namespace Server
+{
+    class LogWriter
+    {
+
+
+
+        public static void WriteLog(string strLog)
+        {
+            StreamWriter log;
+            FileStream fileStream = null;
+            DirectoryInfo logDirInfo = null;
+            FileInfo logFileInfo;
+
+            string logFilePath = "C:\\Logs\\";
+            logFilePath = logFilePath + "Log-" + System.DateTime.Today.ToString("MM-dd-yyyy") + "." + "txt";
+            logFileInfo = new FileInfo(logFilePath);
+            logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+            if (!logDirInfo.Exists) logDirInfo.Create();
+            if (!logFileInfo.Exists)
+            {
+                fileStream = logFileInfo.Create();
+            }
+            else
+            {
+                fileStream = new FileStream(logFilePath, FileMode.Append);
+            }
+            log = new StreamWriter(fileStream);
+            log.WriteLine(strLog);
+            log.Close();
+        }
+    }
+}
